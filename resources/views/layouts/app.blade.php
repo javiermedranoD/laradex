cc5d957ade64ace372e5dfbce3b7e260d89c2316
Javier Medrano <javierlmedrano@gmail.com>
2018-05-14 13:54:41 -0500

    Add navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+  <div class="navbar navbar-dark bg-primary">
+    <a href="" class="navbar-brand">LaraDex</a>
+  </div>
   <div class="container">
       @yield('content')
   </div>
