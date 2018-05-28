495ed1b468521fa43e3a4f36a80985e9b99a51bd
Javier Medrano <javierlmedrano@gmail.com>
2018-05-27 21:07:21 -0500

    Add style to index view

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -6,18 +6,18 @@
 <h2>Listado de trainers</h2><hr>
 
 {{-- <div class="row"> --}}
-  <div class="card-columns">
+  <div class="row">
     @foreach ($trainers as $trainer)
-        {{-- <div class="col-sm"> --}}
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="images/{{ $trainer->avatar }}" alt="Card image cap">
+        <div class="col-sm">
+          <div class="card text-center" style="width: 18rem; margin-top:20px;">
+            <img style="height:100px; width: 100px; background-color: #EFEFEF; margin:10px;" class="card-img-top rounded-circle mx-auto d-block border border-info" src="images/{{ $trainer->avatar }}" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">{{ $trainer->name }}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <p class="card-text">{{ $trainer->description }}</p>
+              <a href="/trainers/{{$trainer->id}}" class="btn btn-primary">Ver más</a>
             </div>
           </div>
-        {{-- </div> --}}
+        </div>
     @endforeach
   </div>
 {{-- </div> --}}
